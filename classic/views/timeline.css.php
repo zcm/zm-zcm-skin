aa9a6d1d2b86a81416ae50199be305f2ca231f8e
Zachary Murray <709873+zcm@users.noreply.github.com>
2022-04-13 16:58:37 -0700

    Initial commit as copy of classic skin from v1.37.13

---
+++ classic/views/timeline.css.php
@@ -0,0 +1,41 @@
+.chartSize {
+    width: <?php echo $chart['width'] ?>px;
+    height: <?php echo $chart['height'] ?>px;
+}
+
+.graphSize {
+    width: <?php echo $chart['graph']['width'] ?>px;
+    height: <?php echo $chart['graph']['height'] ?>px;
+}
+
+.graphHeight {
+    height: <?php echo $chart['graph']['height'] ?>px;
+}
+
+.graphWidth {
+    width: <?php echo $chart['graph']['width'] ?>px;
+}
+
+.imageSize {
+    width: <?php echo $chart['image']['width'] ?>px;
+    height: <?php echo $chart['image']['height'] ?>px;
+}
+
+.imageHeight {
+    height: <?php echo $chart['image']['height'] ?>px;
+}
+
+.activitySize {
+    width: <?php echo $chart['graph']['width'] ?>px;
+    height: <?php echo $chart['graph']['activityHeight'] ?>px;
+}
+
+.eventsSize {
+    width: <?php echo $chart['graph']['width'] ?>px;
+    height: <?php echo $chart['graph']['eventBarHeight'] ?>px;
+}
+
+.events .event {
+    height: <?php echo $chart['graph']['eventBarHeight'] ?>px;
+}
+?>
